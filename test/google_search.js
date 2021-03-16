74fa8f7d0ec8290a73641cf9e8a2932bc576dae7
buivanquy <quy.buivan@powergatesoftware.com>
2021-03-16 16:04:17 +0700

    fix bug in google search

--- test/google_search.js
+++ test/google_search.js
@@ -12,7 +12,8 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 		.build();
   try {
     await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN);
+    await driver.findElement(By.name('q'));
+    await driver.sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     await driver.quit();

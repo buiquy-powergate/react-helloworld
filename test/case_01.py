0ff71887604277dde51f05387605e876f80a2860
buivanquy <quy.buivan@powergatesoftware.com>
2021-03-16 17:18:55 +0700

    fix in linux environment

--- test/case_01.py
+++ test/case_01.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 #chrome_options.add_argument("--disable-extensions")
 #chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage") 
 chrome_options.add_argument("--headless")
 # chrome_options.headless = True # also works
 driver = webdriver.Chrome(options=chrome_options)

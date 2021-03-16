c4abc2cc315076ac90a7b1a933cac4239aba5a92
buivanquy <quy.buivan@powergatesoftware.com>
2021-03-16 15:37:36 +0700

    add google_search.js

---
+++ test/google_search.js
@@ -0,0 +1,20 @@
+const screen = {
+  width: 640,
+  height: 480
+};
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder()
+		.forBrowser('selenium-webdriver/chrome')
+		.setChromeOptions(new chrome.Options().headless().windowSize(screen))
+		.build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
